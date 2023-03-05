500f9548618d84b71c520b658cd37734396d2d26
Andy-Python-Programmer <Andy-Python-Programmer@users.noreply.github.com>
2023-03-05 07:01:34 +0000

    deploy: 9ad9db89db85d6c897676a6d4d3aaecd6186681d

--- implementors/core/borrow/trait.Borrow.js
+++ implementors/core/borrow/trait.Borrow.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {
 "aero_kernel":[],
-"arrayvec":[["impl&lt;const CAP: usize&gt; Borrow&lt;str&gt; for <a class=\"struct\" href=\"arrayvec/struct.ArrayString.html\" title=\"struct arrayvec::ArrayString\">ArrayString</a>&lt;CAP&gt;"],["impl&lt;T, const CAP: usize&gt; Borrow&lt;[T]&gt; for <a class=\"struct\" href=\"arrayvec/struct.ArrayVec.html\" title=\"struct arrayvec::ArrayVec\">ArrayVec</a>&lt;T, CAP&gt;"]],
+"arrayvec":[["impl&lt;T, const CAP: usize&gt; Borrow&lt;[T]&gt; for <a class=\"struct\" href=\"arrayvec/struct.ArrayVec.html\" title=\"struct arrayvec::ArrayVec\">ArrayVec</a>&lt;T, CAP&gt;"],["impl&lt;const CAP: usize&gt; Borrow&lt;str&gt; for <a class=\"struct\" href=\"arrayvec/struct.ArrayString.html\" title=\"struct arrayvec::ArrayString\">ArrayString</a>&lt;CAP&gt;"]],
 "intrusive_collections":[["impl&lt;T: ?Sized&gt; Borrow&lt;T&gt; for <a class=\"struct\" href=\"intrusive_collections/struct.UnsafeRef.html\" title=\"struct intrusive_collections::UnsafeRef\">UnsafeRef</a>&lt;T&gt;"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
